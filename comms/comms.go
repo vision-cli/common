a104751f5f460062dd5f082d3a5a2634044b06eb
Maarten Hazewinkel <maarten.hazewinkel@gmail.com>
2023-07-17 16:18:04 +0200

    Add support for improved development workflow (#6)
    * Add support for improved development workflow
    
    Support a monolithic build with embedded plugins, triggered by the 'vision_dev' build tag. This allows running the full vision-cli system including plugins in a single debuggable executable.
    
    * Increase unit test coverage

--- comms/comms.go
+++ comms/comms.go
@@ -8,37 +8,48 @@ import (
 	api_v1 "github.com/vision-cli/api/v1"
 	"github.com/vision-cli/common/execute"
 	"github.com/vision-cli/common/marshal"
+	"github.com/vision-cli/common/plugins"
+	"github.com/vision-cli/common/tmpl"
 )
 
-func Call[T any](plugin string, request *api_v1.PluginRequest, executor execute.Executor) (*T, error) {
+func Call[T any](plugin plugins.Plugin, request *api_v1.PluginRequest, executor execute.Executor) (*T, error) {
 	if request == nil {
 		return nil, fmt.Errorf("comms.Call called with nil request")
 	}
 	if executor == nil {
 		return nil, fmt.Errorf("comms.Call called with nil executor")
 	}
-	cmd := exec.Command(plugin)
+	cmd := exec.Command(plugin.PluginPath)
 	query, err := marshal.Marshal(request)
 	if err != nil {
-		return nil, fmt.Errorf("cannot marshal request for plugin %s: %s", plugin, err.Error())
+		return nil, fmt.Errorf("cannot marshal request for plugin %s: %s", plugin.Name, err.Error())
 	}
 	cmd.Stdin = strings.NewReader(query)
-	response, err := executor.Output(cmd, ".", "calling plugin "+plugin)
-	if err != nil {
-		return nil, fmt.Errorf("cannot run plugin %s", plugin)
+
+	var response string
+	if plugin.InternalCommand == nil {
+		execResponse, err := executor.Output(cmd, ".", "calling plugin "+plugin.Name)
+		if err != nil {
+			return nil, fmt.Errorf("cannot run plugin %s", plugin.Name)
+		}
+		response = execResponse
+	} else {
+		templateWriter := tmpl.NewOsTmpWriter()
+		response = plugin.InternalCommand(query, executor, templateWriter)
 	}
+
 	out, err := marshal.Unmarshal[T](response)
 	if err != nil {
 		// check if the response is an error
 		outerr, err := marshal.Unmarshal[api_v1.PluginResponse](response)
 		if err != nil {
-			return nil, fmt.Errorf("cannot unmarshal response from plugin %s: %s", plugin, err.Error())
+			return nil, fmt.Errorf("cannot unmarshal response from plugin %s: %s", plugin.Name, err.Error())
 		}
 		if outerr.Error != "" {
 			return nil, fmt.Errorf(outerr.Error)
 		}
 		return nil,
-			fmt.Errorf("did not get expected result type from %s:, got PluginResponse with result %s", plugin, outerr.Result)
+			fmt.Errorf("did not get expected result type from %s:, got PluginResponse with result %s", plugin.Name, outerr.Result)
 	}
 	return &out, nil
 }
